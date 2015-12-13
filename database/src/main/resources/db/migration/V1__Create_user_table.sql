660b69f45537e96bff69b130cd5384e36fbe72c8
sidorow.artem <sidorow.artem@gmail.com>
2015-12-13 12:31:46 +0200

    Database-module configuration commit

---
+++ database/src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,4 @@
+create table user_info (
+  user_id   serial primary key,
+  user_name varchar(255) not null
+);
---
+++ database/src/main/resources/db/migration/V2__add_some_users.sql
@@ -0,0 +1,3 @@
+insert into user_info(user_name) values('Vasja Pupkin');
+insert into user_info(user_name) values('test');
+insert into user_info(user_name) values('root');
